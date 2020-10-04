b1ca4d90124be60c113cb498a6265032634199f5
limwoobin <drogba02@naver.com>
2020-10-04 21:18:31 +0900

    Observer Pattern
    News example

---
+++ src/ObserverPattern/ex1/Observer.java
@@ -0,0 +1,5 @@
+package ObserverPattern.ex1;
+
+public interface Observer {
+    public void update(String title , String news);
+}
---
+++ src/ObserverPattern/ex1/Publisher.java
@@ -0,0 +1,7 @@
+package ObserverPattern.ex1;
+
+public interface Publisher {
+    public void add(Observer observer);
+    public void delete(Observer observer);
+    public void notifyObserver();
+}
