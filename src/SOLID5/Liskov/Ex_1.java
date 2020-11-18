6784564adc41c12d31d9652a56ab56d62ccf3e33
limwoobin <drogba02@naver.com>
2020-11-18 15:42:03 +0900

    SOLID
    - 리스코프 치환 원칙 Ex

---
+++ src/SOLID5/Liskov/Ex_1.java
@@ -0,0 +1,52 @@
+package SOLID5.Liskov;
+
+// 잘못된 예제
+public class Ex_1 {
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle();
+        Rectangle square = new Square();
+
+        System.out.println(getArea(rectangle) == 200);
+        System.out.println(getArea(square) == 100);
+    }
+
+    private static int getArea(Rectangle rectangle) {
+        rectangle.setHeight(10);
+        rectangle.setWidth(20);
+
+        return rectangle.getArea();
+    }
+}
+
+class Rectangle {
+    protected int width;
+    protected int height;
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getArea() {
+        return width * height;
+    }
+}
+
+class Square extends Rectangle {
+
+    @Override
+    public int getArea() {
+        return width * width;
+    }
+}
---
+++ src/SOLID5/Liskov/Ex_2.java
@@ -0,0 +1,64 @@
+package SOLID5.Liskov;
+
+// 수정된 코드
+public class Ex_2 {
+    public static void main(String[] args) {
+        Shape rectangle = new Rectangle2(10 , 20);
+        Shape square = new Square2(10);
+
+        System.out.println(getArea(rectangle));
+        System.out.println(getArea(square));
+    }
+
+    private static int getArea(Shape shape) {
+        return shape.getArea();
+    }
+}
+
+abstract class Shape {
+    abstract int getArea();
+}
+
+class Rectangle2 extends Shape {
+    protected int width;
+    protected int height;
+
+    public Rectangle2(int width , int hegith) {
+        this.width = width;
+        this.height = hegith;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    @Override
+    public int getArea() {
+        return width * height;
+    }
+}
+
+class Square2 extends Shape {
+    protected int width;
+
+    public Square2(int width) {
+        this.width = width;
+    }
+
+    @Override
+    int getArea() {
+        return width * width;
+    }
+}
