6c5b3f1aece2028007f1cc6827e2c3bf65dd5a3d
limwoobin <drogba02@naver.com>
2020-11-27 16:16:08 +0900

    AdapterPattern
    - Duck , Turkey example...

---
+++ src/AdapterPattern/ex1/Duck.java
@@ -0,0 +1,6 @@
+package AdapterPattern.ex1;
+
+public interface Duck {
+    public void quack();
+    public void fly();
+}
---
+++ src/AdapterPattern/ex1/Turkey.java
@@ -0,0 +1,6 @@
+package AdapterPattern.ex1;
+
+public interface Turkey {
+    public void gobble();
+    public void fly();
+}
