8ca2b2c893060c7433b9f2adff7fe59d9e9e66d6
woobeen.lim <drogba02@naver.com>
2021-03-19 13:27:44 +0900

    객체지향 생활체조 9 원칙
    5. 줄여쓰지 않는다

---
+++ src/객체지향생활체조_9원칙/No5/Example.java
@@ -0,0 +1,15 @@
+package 객체지향생활체조_9원칙.No5;
+
+public class Example {
+
+    void as_is() {
+        String eName = "woobeem.lim";
+        String kName = "임우빈";
+    }
+
+    void to_be() {
+        String englishName = "woobeen.lim";
+        String koreanName = "임우빈";
+    }
+}
+
